25e0eee34b5307c61268d1e6870981a11259f1b5
Lust3r <lust3r@Lust3rui-MacBookAir.local>
2024-01-16 18:19:22 +0900

    [File] Create Xcode Project

---
+++ SeoulCultureMoa/SeoulCultureMoa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SeoulCultureMoa
+//
+//  Created by Lust3r on 1/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SeoulCultureMoa/SeoulCultureMoa/SeoulCultureMoaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeoulCultureMoaApp.swift
+//  SeoulCultureMoa
+//
+//  Created by Lust3r on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SeoulCultureMoaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
