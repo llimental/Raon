ddfeb5b0bd40281f5f24192f5022e519d7da36d2
Lust3r <ideal_lust3r@icloud.com>
2024-01-19 20:23:26 +0900

    [Feat] #1 InitialContentView 파일 생성 및 WindowGroup 변경

--- SeoulCultureMoa/SeoulCultureMoa/SeoulCultureMoaApp.swift
+++ SeoulCultureMoa/SeoulCultureMoa/SeoulCultureMoaApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SeoulCultureMoaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            InitialContentView()
         }
     }
 }
---
+++ SeoulCultureMoa/SeoulCultureMoa/Views/InitialViews/InitialContentView.swift
@@ -0,0 +1,17 @@
+//
+//  InitialContentView.swift
+//  SeoulCultureMoa
+//
+//  Created by Lust3r on 1/19/24.
+//
+
+import SwiftUI
+
+struct InitialContentView: View {
+    var body: some View {
+    }
+}
+
+#Preview {
+    InitialContentView()
+}
