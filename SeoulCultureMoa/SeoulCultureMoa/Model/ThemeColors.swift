0d4189c85a283008477f052a471b47efe7efa2f5
Lust3r <ideal_lust3r@icloud.com>
2024-01-27 01:21:05 +0900

    [Feat] #1 ThemeColors enum 생성
    - 앱 테마 색상을 enum 케이스를 통해 분기하고, rawValue를 통해 asset에 접근하기 위함

---
+++ SeoulCultureMoa/SeoulCultureMoa/Model/ThemeColors.swift
@@ -0,0 +1,13 @@
+//
+//  ThemeColors.swift
+//  SeoulCultureMoa
+//
+//  Created by Lust3r on 1/27/24.
+//
+
+import Foundation
+
+enum ThemeColors: String {
+    case pink = "AccentColor_Pink"
+    case blue = "AccentColor_Blue"
+}
