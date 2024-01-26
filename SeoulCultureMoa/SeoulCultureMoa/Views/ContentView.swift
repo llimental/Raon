0027a3624a76a14429d7c3f7db4b3874b8647330
Lust3r <ideal_lust3r@icloud.com>
2024-01-27 01:46:39 +0900

    [Feat] #1 AppStorage에 Bool 타입 값을 갖는 isFirstLaunching키 생성
    - OnboardingPage를 보여주는 기준으로 사용할 isFirstLaunching 데이터를 AppStorage에 저장

--- SeoulCultureMoa/SeoulCultureMoa/Views/ContentView.swift
+++ SeoulCultureMoa/SeoulCultureMoa/Views/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("isFirstLaunching") private var isFirstLaunching: Bool = true
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
