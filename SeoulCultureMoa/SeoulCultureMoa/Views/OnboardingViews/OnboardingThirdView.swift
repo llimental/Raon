342abe3dd8dd76a4627b2e230c8ea51753f0a041
Lust3r <ideal_lust3r@icloud.com>
2024-01-27 01:59:22 +0900

    [Feat] #1 OnboardingThirdView 구현
    - Text와 ColoredButton을 활용한 레이아웃 구성

--- SeoulCultureMoa/SeoulCultureMoa/Views/OnboardingViews/OnboardingThirdView.swift
+++ SeoulCultureMoa/SeoulCultureMoa/Views/OnboardingViews/OnboardingThirdView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct OnboardingThirdView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+
+                Text("반갑습니다!")
+                    .font(.title)
+                    .foregroundStyle(Color(ThemeColors.pink.rawValue))
+                    .fontWeight(.bold)
+                    .padding(.bottom, 20)
+
+                Text("다양한 문화행사 정보를 둘러보세요")
+                    .font(.callout)
+                    .padding(.bottom, 40)
+
+                Spacer()
+
+                ColoredButton(
+                    buttonAction: {
+
+                    },
+                    buttonText: "확인",
+                    buttonColor: ThemeColors.pink,
+                    buttonWidth: geometry.size.width * 0.7)
+            }
+            .padding()
+            .frame(width: geometry.size.width)
         }
-        .padding()
     }
 }
 
