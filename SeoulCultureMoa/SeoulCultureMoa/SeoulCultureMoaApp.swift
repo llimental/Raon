9bff21502a61e8fe489033474b3de8ad9b448f36
Lust3r <ideal_lust3r@icloud.com>
2024-01-27 01:25:03 +0900

    [Refactor] #1 WindowGroup의 인스턴스 변경
    - OnboardingFirstView -> ContentView

--- SeoulCultureMoa/SeoulCultureMoa/SeoulCultureMoaApp.swift
+++ SeoulCultureMoa/SeoulCultureMoa/SeoulCultureMoaApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SeoulCultureMoaApp: App {
     var body: some Scene {
         WindowGroup {
-            OnboardingFirstView()
+            ContentView()
         }
     }
 }
