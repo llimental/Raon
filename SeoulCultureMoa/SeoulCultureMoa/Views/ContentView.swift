53fc81d04063c6210f686b6995b05f8fae952c08
Lust3r <ideal_lust3r@icloud.com>
2024-01-27 02:10:00 +0900

    [Feat] #1 AppStorage에 ThemeColors 타입 값을 갖는 ThemeColor키 생성
    - enum인 ThemeColor의 값을 저장할 수 있는 이유는, enum이 rawRepresentable을 준수하고, AppStorage는 그러한 rawRepresentable 타입을 지원하기 때문

--- SeoulCultureMoa/SeoulCultureMoa/Views/ContentView.swift
+++ SeoulCultureMoa/SeoulCultureMoa/Views/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @AppStorage("isFirstLaunching") private var isFirstLaunching: Bool = true
+    @AppStorage("ThemeColor") var themeColor: ThemeColors = .pink
 
     var body: some View {
         Text("App Main")
