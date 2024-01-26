7d8b493c44f4bd14ad2c96639392dee7b6ba185f
Lust3r <ideal_lust3r@icloud.com>
2024-01-27 01:41:28 +0900

    [Feat] #1 ColoredButton 구현
    - 같은 스타일을 가진 버튼의 재사용성을 위해 Subview로 모듈화

---
+++ SeoulCultureMoa/SeoulCultureMoa/Views/ButtonViews/ColoredButton.swift
@@ -0,0 +1,39 @@
+//
+//  ColoredButton.swift
+//  SeoulCultureMoa
+//
+//  Created by Lust3r on 1/26/24.
+//
+
+import SwiftUI
+
+struct ColoredButton: View {
+    let buttonAction: () -> Void
+    let buttonText: String
+    let buttonColor: ThemeColors
+
+    var buttonWidth: CGFloat? = nil
+    var buttonHeight: CGFloat? = nil
+
+    var body: some View {
+        Button(action: {
+            buttonAction()
+        }, label: {
+            Text(buttonText)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+                .padding()
+                .frame(width: buttonWidth, height: buttonHeight)
+                .background(Color(buttonColor.rawValue))
+                .clipShape(.buttonBorder)
+        })
+    }
+}
+
+#Preview {
+    ColoredButton(
+        buttonAction: { },
+        buttonText: "Button",
+        buttonColor: .pink
+    )
+}
