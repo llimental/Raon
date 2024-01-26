9482b6daf50a88631fa06ae3c8532ce232493a86
Lust3r <ideal_lust3r@icloud.com>
2024-01-27 01:34:00 +0900

    [Feat] #1 OnboardingSecond, ThirdView 초기 구현

---
+++ SeoulCultureMoa/SeoulCultureMoa/Views/OnboardingViews/OnboardingSecondView.swift
@@ -0,0 +1,24 @@
+//
+//  OnboardingSecondView.swift
+//  SeoulCultureMoa
+//
+//  Created by Lust3r on 1/23/24.
+//
+
+import SwiftUI
+
+struct OnboardingSecondView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    OnboardingSecondView()
+}
---
+++ SeoulCultureMoa/SeoulCultureMoa/Views/OnboardingViews/OnboardingThirdView.swift
@@ -0,0 +1,24 @@
+//
+//  OnboardingThirdView.swift
+//  SeoulCultureMoa
+//
+//  Created by Lust3r on 1/23/24.
+//
+
+import SwiftUI
+
+struct OnboardingThirdView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    OnboardingThirdView()
+}
