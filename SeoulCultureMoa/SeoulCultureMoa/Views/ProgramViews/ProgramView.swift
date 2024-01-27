7af078d5ad33707355aae2df01037051ffb096f2
Lust3r <ideal_lust3r@icloud.com>
2024-01-27 17:34:01 +0900

    [File] #2 ProgramView 파일 생성
    - 메인 화면을 보여줄 View

---
+++ SeoulCultureMoa/SeoulCultureMoa/Views/ProgramViews/ProgramView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgramView.swift
+//  SeoulCultureMoa
+//
+//  Created by Lust3r on 1/27/24.
+//
+
+import SwiftUI
+
+struct ProgramView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgramView()
+}
