70bc5f1e7e10785fbc805a98c41a51913ae63ae7
Lust3r <ideal_lust3r@icloud.com>
2024-01-31 01:14:28 +0900

    [Feat] #2 ProgramView 레이아웃 조정
    - GeometryReader를 사용하여 하드코딩된 숫자 대신 기기 사이즈 비례 사이즈로 frame 지정
    - ProgramCardView의 Frame 지정

--- SeoulCultureMoa/SeoulCultureMoa/Views/ProgramViews/ProgramView.swift
+++ SeoulCultureMoa/SeoulCultureMoa/Views/ProgramViews/ProgramView.swift
@@ -13,41 +13,47 @@ struct ProgramView: View {
     @StateObject private var networkManager = NetworkManager()
 
     var body: some View {
-        NavigationStack {
-            Divider()
-
-            ScrollView(.horizontal) {
-                LazyHStack(spacing: 20) {
-                    ForEach(networkManager.contents.sorted { $0.startDate < $1.startDate }, id: \.title) { content in
-                        ProgramCardView(
-                            themeColor: $themeColor,
-                            programTitle: content.title,
-                            programImageURL: content.imageURL
-                        )
+        GeometryReader { geometry in
+            NavigationStack {
+                ScrollView(.horizontal) {
+                    LazyHStack(spacing: 20) {
+                        ForEach(networkManager.contents.sorted { $0.startDate < $1.startDate }, id: \.title) { content in
+                            ProgramCardView(
+                                themeColor: $themeColor,
+                                programTitle: content.title,
+                                programImageURL: content.imageURL
+                            )
+                            .frame(width: geometry.size.width * 0.7)
+                            .padding()
+                        }
                     }
                 }
-            }
-            .scrollIndicators(.hidden)
-            .navigationTitle("프로그램")
-            .navigationBarTitleTextColor(Color(themeColor.rawValue))
-            .toolbar {
-                ToolbarItemGroup(placement: .topBarTrailing) {
-                    NavigationLink {
+                .scrollIndicators(.hidden)
+                .frame(height: geometry.size.height * 0.8)
+                .navigationTitle("프로그램")
+                .navigationBarTitleTextColor(Color(themeColor.rawValue))
+                .toolbar {
+                    ToolbarItemGroup(placement: .topBarTrailing) {
+                        NavigationLink {
 
-                    } label: {
-                        Image(systemName: "calendar")
-                    }
+                        } label: {
+                            Image(systemName: "calendar")
+                        }
 
-                    NavigationLink {
+                        NavigationLink {
 
-                    } label: {
-                        Image(systemName: "magnifyingglass")
+                        } label: {
+                            Image(systemName: "magnifyingglass")
+                        }
                     }
                 }
+                .tint(Color(themeColor.rawValue))
+                .padding()
             }
-            .padding(.vertical)
+            .frame(
+                width: geometry.size.width,
+                height: geometry.size.height)
         }
-        .tint(Color(themeColor.rawValue))
     }
 }
 
