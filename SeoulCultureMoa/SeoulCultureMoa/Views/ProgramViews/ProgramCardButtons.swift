e81e3182ff59497ff135def0a5f5ed49d38a8e33
Lust3r <ideal_lust3r@icloud.com>
2024-01-30 22:10:48 +0900

    [Feat] #2 CardButtons SubView 분리
    - 기존의 ProgramCard에서 직접 HStack을 통해 만들었던 Button 모음을 따로 분리하고, 가져와 사용하는 것으로 모듈화

---
+++ SeoulCultureMoa/SeoulCultureMoa/Views/ProgramViews/ProgramCardButtons.swift
@@ -0,0 +1,40 @@
+//
+//  ProgramCardButtons.swift
+//  SeoulCultureMoa
+//
+//  Created by Lust3r on 1/30/24.
+//
+
+import SwiftUI
+
+struct ProgramCardButtons: View {
+    @Binding var themeColor: ThemeColors
+
+    var body: some View {
+        HStack(spacing: 20) {
+            Button(action: {
+
+            }, label: {
+                Image(systemName: "heart")
+                    .font(.system(size: 30))
+            })
+
+            ColoredButton(
+                buttonAction: {
+
+                },
+                buttonText: "바로가기",
+                buttonColor: themeColor,
+                buttonWidth: 150
+            )
+
+            Button(action: {
+
+            }, label: {
+                Image(systemName: "square.and.arrow.up")
+                    .font(.system(size: 30))
+            })
+        }
+        .foregroundStyle(Color(themeColor.rawValue))
+    }
+}
