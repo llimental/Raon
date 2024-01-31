32cf56954cc4d55d9cab8f21bb43e8be80ab501d
Lust3r <ideal_lust3r@icloud.com>
2024-02-01 01:03:11 +0900

    [Feat] #2 ProgramView 구성 ScrollView에서 TabView로 변경
    - 실 사용시 스크롤을 하면 부드럽지만, 중간에 걸치게 되고 제대로 보려면 조정하는 소요가 있어 깔끔하게 페이징 할 수 있도록 page Style의 TabView로 변경

--- SeoulCultureMoa/SeoulCultureMoa/Views/ProgramViews/ProgramView.swift
+++ SeoulCultureMoa/SeoulCultureMoa/Views/ProgramViews/ProgramView.swift
@@ -13,46 +13,37 @@ struct ProgramView: View {
     @StateObject private var networkManager = NetworkManager()
 
     var body: some View {
-        GeometryReader { geometry in
-            NavigationStack {
-                ScrollView(.horizontal) {
-                    LazyHStack(spacing: 20) {
-                        ForEach(networkManager.contents.sorted { $0.startDate < $1.startDate }, id: \.title) { content in
-                            ProgramCardView(
-                                themeColor: $themeColor,
-                                programTitle: content.title,
-                                programImageURL: content.imageURL
-                            )
-                            .frame(width: geometry.size.width * 0.7)
-                            .padding()
-                        }
-                    }
+        NavigationStack {
+            TabView {
+                ForEach(networkManager.contents, id: \.title) { content in
+                    ProgramCardView(
+                        themeColor: $themeColor,
+                        programTitle: content.title,
+                        programImageURL: content.imageURL
+                    )
+                    .padding()
                 }
-                .scrollIndicators(.hidden)
-                .frame(height: geometry.size.height * 0.8)
-                .navigationTitle("프로그램")
-                .navigationBarTitleTextColor(Color(themeColor.rawValue))
-                .toolbar {
-                    ToolbarItemGroup(placement: .topBarTrailing) {
-                        NavigationLink {
-
-                        } label: {
-                            Image(systemName: "calendar")
-                        }
-
-                        NavigationLink {
-
-                        } label: {
-                            Image(systemName: "magnifyingglass")
-                        }
-                    }
-                }
-                .tint(Color(themeColor.rawValue))
-                .padding()
             }
-            .frame(
-                width: geometry.size.width,
-                height: geometry.size.height)
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .navigationTitle("프로그램")
+            .navigationBarTitleTextColor(Color(themeColor.rawValue))
+            .toolbar {
+                ToolbarItemGroup(placement: .topBarTrailing) {
+                    NavigationLink {
+
+                    } label: {
+                        Image(systemName: "calendar")
+                    }
+
+                    NavigationLink {
+
+                    } label: {
+                        Image(systemName: "magnifyingglass")
+                    }
+                }
+            }
+            .tint(Color(themeColor.rawValue))
+            .padding()
         }
     }
 }
