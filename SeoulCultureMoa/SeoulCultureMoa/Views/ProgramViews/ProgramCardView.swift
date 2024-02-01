caba2e6ce31c1b3d5cb0642cfa9f29d60489a8a9
Lust3r <ideal_lust3r@icloud.com>
2024-02-02 00:07:48 +0900

    [Feat] #2 ProgramCardView내 Text, CardButtons 제거
    - 제목 길이에 따른 레이아웃의 불균형 등으로 인해 기존 View 구성요소 제거
    - 포토카드처럼 프로그램 포스터만 보여주는 것이 더 낫다는 판단
    - 기존 제목 텍스트와 버튼모음은 카드 뒷면에 추가 예정

--- SeoulCultureMoa/SeoulCultureMoa/Views/ProgramViews/ProgramCardView.swift
+++ SeoulCultureMoa/SeoulCultureMoa/Views/ProgramViews/ProgramCardView.swift
@@ -8,8 +8,6 @@
 import SwiftUI
 
 struct ProgramCardView: View {
-    @Binding var themeColor: ThemeColors
-
     let programTitle: String
     let programImageURL: String
 
@@ -25,31 +23,17 @@ struct ProgramCardView: View {
                 })
             .scaledToFit()
             .clipShape(.rect(cornerRadius: 15))
-            .padding(.bottom, 20)
-
-            Text(programTitle)
-                .font(.title3)
-                .fontWeight(.bold)
-                .allowsTightening(true)
-                .multilineTextAlignment(.center)
-                .frame(height: 50)
-                .padding(.bottom, 50)
-
-            ProgramCardButtons(themeColor: $themeColor)
         }
-        .padding(20)
         .background(
             RoundedRectangle(cornerRadius: 15)
-                .foregroundStyle(Color(themeColor.rawValue))
                 .shadow(color: .primary, radius: 3, x: 7, y: 7)
-                .opacity(0.2)
+                .opacity(0.5)
         )
     }
 }
 
 #Preview {
     ProgramCardView(
-        themeColor: .constant(.pink),
         programTitle: "프로그램 제목",
         programImageURL: ""
     )
