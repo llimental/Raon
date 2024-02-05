e07749fb764b663f47f846460ded580a1864c3d4
Lust3r <ideal_lust3r@icloud.com>
2024-02-06 00:52:00 +0900

    [Feat] #3 hideKeyboard 메서드 구현
    - resignFirstResponder를 수행하는 메서드

--- SeoulCultureMoa/SeoulCultureMoa/Extensions/View+Extension.swift
+++ SeoulCultureMoa/SeoulCultureMoa/Extensions/View+Extension.swift
@@ -16,4 +16,12 @@ extension View {
 
         return self
     }
+
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(
+            #selector(UIResponder.resignFirstResponder),
+            to: nil,
+            from: nil,
+            for: nil)
+    }
 }
