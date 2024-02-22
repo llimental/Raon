1ffb2ab1805bb803c125322cda84d1a36157c6b9
Lust3r <ideal_lust3r@icloud.com>
2024-02-22 14:38:32 +0900

    [Feat] #4 ProgramDetailView 파일 생성

---
+++ SeoulCultureMoa/SeoulCultureMoa/Views/ProgramDetailViews/ProgramDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgramDetailView.swift
+//  SeoulCultureMoa
+//
+//  Created by Lust3r on 2/22/24.
+//
+
+import SwiftUI
+
+struct ProgramDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgramDetailView()
+}
