1afc42f4804bf08d8cb6efcf6c24f5fb88c44727
Lust3r <ideal_lust3r@icloud.com>
2024-02-24 15:15:26 +0900

    [Feat] #4 cornerRadius 메서드 구현
    - View를 RoundedCorner의 모양으로 변환하여 반환하는 확장 메서드

--- SeoulCultureMoa/SeoulCultureMoa/Extensions/View+Extension.swift
+++ SeoulCultureMoa/SeoulCultureMoa/Extensions/View+Extension.swift
@@ -24,4 +24,8 @@ extension View {
             from: nil,
             for: nil)
     }
+
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
 }
