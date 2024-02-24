bda7ea2989c69350b20245743ea003b2fef66db6
Lust3r <ideal_lust3r@icloud.com>
2024-02-24 15:06:36 +0900

    [Feat] #4 배경 이미지로 사용할 AsyncImage View 구현

--- SeoulCultureMoa/SeoulCultureMoa/Views/ProgramDetailViews/ProgramDetailView.swift
+++ SeoulCultureMoa/SeoulCultureMoa/Views/ProgramDetailViews/ProgramDetailView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct ProgramDetailView: View {
+    // MARK: - Public Properties
+    var content: ProgramContent
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        AsyncImage(url: URL(string: content.imageURL)) { phase in
+            if let image = phase.image {
+                image.resizable()
+            } else if phase.error != nil {
+                Color.gray
+            }
+        }
+        .scaledToFit()
+        .opacity(0.5)
     }
 }
 
 #Preview {
-    ProgramDetailView()
+    ProgramDetailView(
+        content: ProgramContent(
+            category: "뮤지컬/오페라",
+            region: "종로구",
+            title: "오페라 갈라",
+            place: "세종대극장",
+            organization: "세종문화회관",
+            target: "7세 이상 관람 가능(2017년생부터 관람 가능)",
+            fees: "SUITE석 140,000원 / VIP석 120,000원 / R석 100,000원 / S석 80,000원 / A석 50,000원 / B석 30,000원",
+            player: "",
+            url: "https://www.sejongpac.or.kr/portal/performance/performance/view.do?menuNo=200004&performIdx=34877",
+            imageURL: "https://culture.seoul.go.kr/cmmn/file/getImage.do?atchFileId=a6cb4e5a9b5b404e83454b084d88a399&thumb=Y",
+            startDate: "2024-12-07",
+            endDate: "2024-12-07",
+            longitude: "37.5726241",
+            latitude: "126.9760053",
+            isFree: "유료"))
 }
