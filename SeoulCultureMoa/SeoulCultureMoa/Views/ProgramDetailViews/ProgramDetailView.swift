f7d706032891ebe5d0c4843331d5a8d0d1077063
Lust3r <ideal_lust3r@icloud.com>
2024-02-26 16:39:16 +0900

    [Feat] #4 ProgramDetailView toolbarTitleDisplayMode 설정
    - WebView로 넘어갔을 때 navigationBar 부분 여백이 생겨 .inline을 통해 해결

--- SeoulCultureMoa/SeoulCultureMoa/Views/ProgramDetailViews/ProgramDetailView.swift
+++ SeoulCultureMoa/SeoulCultureMoa/Views/ProgramDetailViews/ProgramDetailView.swift
@@ -79,6 +79,7 @@ struct ProgramDetailView: View {
                 }
                 .ignoresSafeArea()
                 .background(.black)
+                .toolbarTitleDisplayMode(.inline)
             }
         }
     }
