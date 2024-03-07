1586f1650368f455f1ed5ce33d0661b97b8ed915
Lust3r <ideal_lust3r@icloud.com>
2024-03-08 00:43:59 +0900

    [Feat] #9 SettingsView List 틀 구현

--- SeoulCultureMoa/SeoulCultureMoa/Views/SettingsViews/SettingsView.swift
+++ SeoulCultureMoa/SeoulCultureMoa/Views/SettingsViews/SettingsView.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section {
+                Text("저작권")
+                Text("문의하기")
+            } header: {
+                Text("정보")
+            }
+
+            Section {
+                Text("테마")
+                Text("관심지역")
+            } header: {
+                Text("개인화")
+            }
+
+        }
+        .listStyle(.plain)
+        .navigationTitle("설정")
     }
 }
 
