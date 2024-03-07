1a68e6edcb3204a5ccf4577e5fd5b0d6cf00b8cf
Lust3r <ideal_lust3r@icloud.com>
2024-03-08 00:33:45 +0900

    [Feat] #9 SettingsView 폴더 및 파일 생성

---
+++ SeoulCultureMoa/SeoulCultureMoa/Views/SettingsViews/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SeoulCultureMoa
+//
+//  Created by Lust3r on 3/8/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
