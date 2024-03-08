018cd2794df152c012676f06fdffd552a554b540
Lust3r <ideal_lust3r@icloud.com>
2024-03-09 00:20:34 +0900

    [Feat] #9 NavigationBarLargeTitleView 구현
    - NavigationTitle 대신 사용 할 LargeTitleView 구현

---
+++ SeoulCultureMoa/SeoulCultureMoa/Views/TextViews/NavigationBarLargeTitleView.swift
@@ -0,0 +1,29 @@
+//
+//  NavigationBarLargeTitleView.swift
+//  SeoulCultureMoa
+//
+//  Created by Lust3r on 3/9/24.
+//
+
+import SwiftUI
+
+struct NavigationBarLargeTitleView: View {
+    // MARK: - Public Properties
+    var titleText: String
+    var themeColor: ThemeColors
+
+    // MARK: - Body
+    var body: some View {
+        Text(titleText)
+            .font(.largeTitle)
+            .bold()
+            .foregroundStyle(themeColor.color)
+            .padding(.leading)
+    }
+}
+
+#Preview {
+    NavigationBarLargeTitleView(
+        titleText: "LargeTitle",
+        themeColor: .pink)
+}
