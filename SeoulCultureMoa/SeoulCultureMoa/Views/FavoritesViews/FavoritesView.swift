560a98a7ef802e89c3be0a1a6377f0e595900fdd
Sang-Yun Lee(Lust3r) <s1skyblue1s@gmail.com>
2024-03-12 16:49:47 +0900

    [Feat] #11 FavoritesView 폴더 및 파일 생성

---
+++ SeoulCultureMoa/SeoulCultureMoa/Views/FavoritesViews/FavoritesView.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesView.swift
+//  SeoulCultureMoa
+//
+//  Created by Lust3r on 3/12/24.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    // MARK: - Body
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
