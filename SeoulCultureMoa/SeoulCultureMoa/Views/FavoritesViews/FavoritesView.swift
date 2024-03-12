5605efcc75a2f30e3c5e222c8220a2081014a597
Sang-Yun Lee(Lust3r) <s1skyblue1s@gmail.com>
2024-03-12 17:08:51 +0900

    [Feat] #11 FavoritesView에 SwiftData 연결 후 Title, List 구현
    - startDate 순서로 정렬된 favoritePrograms 사용

--- SeoulCultureMoa/SeoulCultureMoa/Views/FavoritesViews/FavoritesView.swift
+++ SeoulCultureMoa/SeoulCultureMoa/Views/FavoritesViews/FavoritesView.swift
@@ -6,14 +6,41 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct FavoritesView: View {
+    // MARK: - SwiftData Properties
+    @Environment(\.modelContext) private var modelContext
+    @Query(sort: \FavoriteProgram.content.startDate) private var favoritePrograms: [FavoriteProgram]
+
+    // MARK: - @Binding Properties
+    @Binding var themeColor: ThemeColors
+
     // MARK: - Body
     var body: some View {
-        Text("Hello, World!")
+        NavigationStack {
+            VStack(alignment: .leading) {
+                NavigationBarLargeTitleView(
+                    titleText: "즐겨찾기",
+                    themeColor: themeColor)
+
+                List {
+                    ForEach(favoritePrograms) { favoriteProgram in
+                        NavigationLink {
+                            ProgramDetailView(
+                                themeColor: $themeColor,
+                                content: favoriteProgram.content)
+                        } label: {
+                            ProgramCardView(programImageURL: favoriteProgram.content.imageURL)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    FavoritesView()
+    FavoritesView(themeColor: .constant(.purple))
+        .modelContainer(for: FavoriteProgram.self)
 }
