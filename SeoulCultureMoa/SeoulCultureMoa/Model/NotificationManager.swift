50ee573b2e1c792fb44c23af82d60eb98674cf54
Sang-Yun Lee(Lust3r) <s1skyblue1s@gmail.com>
2024-03-15 01:01:46 +0900

    [Feat] #12 addNotificationRequest 메서드 구현
    - title과 dateString을 받아 UNMutableNotificationContent 구성
    - date에서 timeStamp를 제거하고, 일주일 전 날짜를 얻기 위해 Calendar.current.date(byAdding:value:to:) 메서드를 사용
    - adjustDate를 사용하여 dateComponent를 얻고, 시간을 9시 0분으로 설정
    - dateComponent를 사용하여 UNCalendarNotificationTrigger(dateMatching:repeats:) 생성
    - trigger를 사용하여 UNNotificationRequest(identifier:content:trigger:) 생성
    - 이후 Notification 제거를 위해 identifier는 title로 설정

--- SeoulCultureMoa/SeoulCultureMoa/Model/NotificationManager.swift
+++ SeoulCultureMoa/SeoulCultureMoa/Model/NotificationManager.swift
@@ -11,9 +11,15 @@ import UserNotifications
 final class NotificationManager: ObservableObject {
     // MARK: - Private Properties
     private let center = UNUserNotificationCenter.current()
+    private var dateFormatter: DateFormatter {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
 
-    // MARK: - Public Functions
-    func addNotificationRequest() {
+        return dateFormatter
+    }
+
+    // MARK: - Private Functions
+    func addNotificationRequest(with title: String, when dateString: String) {
         center.getNotificationSettings { [weak self] settings in
             if settings.authorizationStatus != .authorized {
                 self?.center.requestAuthorization(options: [.alert, .sound]) { _, error in
@@ -21,6 +27,27 @@ final class NotificationManager: ObservableObject {
                         print("requestAuthorization Failed(\(error.localizedDescription))")
                     }
                 }
+            }
+        }
+
+        let notificationContent = UNMutableNotificationContent()
+        notificationContent.body = "\(title)이 일주일 뒤에 열려요"
+
+        guard let date = dateFormatter.date(from: dateString.replacingOccurrences(of: " 00:00:00.0", with: "")) else { return }
+
+        guard let adjustDate = Calendar.current.date(byAdding: .day, value: -7, to: date) else { return }
+
+        var dateComponent = Calendar.current.dateComponents([.year, .month, .day], from: adjustDate)
+        dateComponent.hour = 9
+        dateComponent.minute = 0
+
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: false)
+
+        let request = UNNotificationRequest(identifier: title, content: notificationContent, trigger: trigger)
+
+        center.add(request) { error in
+            if let error = error {
+                print("Notification add Failed(\(error.localizedDescription))")
             }
         }
     }
