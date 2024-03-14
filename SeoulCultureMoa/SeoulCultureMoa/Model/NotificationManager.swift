3b63d5dd05b56054b32c536427edc9ed7f944618
Sang-Yun Lee(Lust3r) <s1skyblue1s@gmail.com>
2024-03-15 00:51:52 +0900

    [Feat] #12 NotificationManager 초기 구현
    - current()를 통해 NotificationCenter를 center 프로퍼티에 저장

---
+++ SeoulCultureMoa/SeoulCultureMoa/Model/NotificationManager.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationManager.swift
+//  SeoulCultureMoa
+//
+//  Created by Lust3r on 3/13/24.
+//
+
+import SwiftUI
+import UserNotifications
+
+final class NotificationManager: ObservableObject {
+    // MARK: - Private Properties
+    private let center = UNUserNotificationCenter.current()
+}
