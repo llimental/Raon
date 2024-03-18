59aa74c73d40511d05c557f92caa5c1156151113
Sang-Yun Lee(Lust3r) <s1skyblue1s@gmail.com>
2024-03-19 00:04:57 +0900

    [File] #14 CalendarView 파일 생성

---
+++ SeoulCultureMoa/SeoulCultureMoa/Views/CalendarViews/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  SeoulCultureMoa
+//
+//  Created by Lust3r on 3/19/24.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
