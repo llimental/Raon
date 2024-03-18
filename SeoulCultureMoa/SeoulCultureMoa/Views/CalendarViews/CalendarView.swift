1c259291280fe3ac49db025bea5ead4f9b71fd90
Sang-Yun Lee(Lust3r) <s1skyblue1s@gmail.com>
2024-03-19 01:57:23 +0900

    [Feat] #14 List scrollIndicators visibility 설정

--- SeoulCultureMoa/SeoulCultureMoa/Views/CalendarViews/CalendarView.swift
+++ SeoulCultureMoa/SeoulCultureMoa/Views/CalendarViews/CalendarView.swift
@@ -38,7 +38,7 @@ struct CalendarView: View {
                 }
             }
             .listStyle(.plain)
-            .listRowSeparator(.hidden)
+            .scrollIndicators(.hidden)
         }
         .padding()
     }
