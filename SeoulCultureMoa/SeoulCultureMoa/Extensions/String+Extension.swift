b95b877e702b372d770d7bc605dd62858a3e80db
Sang-Yun Lee(Lust3r) <s1skyblue1s@gmail.com>
2024-03-20 01:37:00 +0900

    [Feat] #14 getDateOfString() 메서드 구현
    - String으로 된 날짜를 Date로 만들어주는 기능
    - 한국시간을 기준으로 할 것이기에 getKSTDate 작업 포함

--- SeoulCultureMoa/SeoulCultureMoa/Extensions/String+Extension.swift
+++ SeoulCultureMoa/SeoulCultureMoa/Extensions/String+Extension.swift
@@ -11,4 +11,13 @@ extension String {
     func removeTimeStamp() -> String {
         return self.replacingOccurrences(of: " 00:00:00.0", with: "")
     }
+
+    func getDateOfString() -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+
+        guard let date = dateFormatter.date(from: self) else { return Date() }
+
+        return date.getKSTDate()
+    }
 }
