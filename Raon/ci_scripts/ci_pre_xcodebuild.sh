b88303bbb0bb5c41967394fef9adf9e05120be09
Sang-Yun Lee(Lust3r) <s1skyblue1s@gmail.com>
2024-03-22 16:22:01 +0900

    [Feat] ci_pre_xcodebuild.sh 생성

---
+++ Raon/ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#  ci_pre_xcodebuild.sh
+#  Raon
+#
+#  Created by Lust3r on 3/22/24.
+#
+
+echo "Stage: PRE-Xcode Build Start"
+
+cd ..
+
+plutil -replace API_KEY -string "$API_KEY" ./Raon/RaonAPI.plist
+
+plutil -p ./Raon/RaonAPI.plist
+
+echo "Stage: PRE-Xcode Build End"
+
+exit 0
