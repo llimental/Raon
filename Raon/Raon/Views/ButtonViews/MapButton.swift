cbbedc70938255208bb4ec13da4c41304158cebb
Sang-Yun Lee(Lust3r) <s1skyblue1s@gmail.com>
2024-04-05 23:44:34 +0900

    [Feat] #16 MapButton에 Apple 지도 추가

--- Raon/Raon/Views/ButtonViews/MapButton.swift
+++ Raon/Raon/Views/ButtonViews/MapButton.swift
@@ -10,6 +10,7 @@ import SwiftUI
 enum MapType {
     case naver
     case kakao
+    case apple
 }
 
 struct MapButton: View {
@@ -40,6 +41,16 @@ struct MapButton: View {
                 }, label: {
                     Text("카카오맵")
                 })
+
+            case .apple:
+                Button(action: {
+                    LinkToAppleMap(
+                        latitude: Double(latitude) ?? 0,
+                        longitude: Double(longitude) ?? 0,
+                        place: place)
+                }, label: {
+                    Text("애플 지도")
+                })
         }
     }
 
@@ -47,7 +58,7 @@ struct MapButton: View {
     private func LinkToNaverMap(latitude: Double, longitude: Double, place: String) {
         // 네이버 지도 앱 연동 URL Scheme: https://guide.ncloud-docs.com/docs/maps-url-scheme
 
-        // 네이버 지도를 실행하고 도착지 좌표(dlat, dlng)까지 대중교통(/route/public) 길찾기
+        // 네이버 지도를 실행하고 전달된 위/경도 장소에 장소명으로 마커 표시
         guard let url = URL(string: "nmap://place?lat=\(latitude)&lng=\(longitude)&name=\(place)&appname=com.lust3r.raon") else { return }
 
         // nmap:// 스킴을 인식하지 못하는 경우 앱스토어 페이지로 연결해 앱 설치를 유도
@@ -65,7 +76,7 @@ struct MapButton: View {
     private func LinkToKakaoMap(latitude: Double, longitude: Double) {
         // Kakao 지도 iOS API 가이드: https://apis.map.kakao.com/ios/guide/#urlscheme_open_mapapp
 
-        // 카카오맵을 실행하고 도착지 좌표(WGS84, ep)까지 대중교통(PUBLICTRANSIT) 길찾기
+        // 카카오맵을 실행하고 전달된 위/경도 장소에 마커 표시
         guard let url = URL(string: "kakaomap://look?p=\(latitude),\(longitude)") else { return }
 
         // kakaomap:// 스킴을 인식하지 못하는 경우 앱스토어 페이지로 연결해 앱 설치를 유도
@@ -77,6 +88,18 @@ struct MapButton: View {
         } else {
             // 카카오맵이 설치되어 있지 않다면 appStoreURL을 연결
             UIApplication.shared.open(appStoreURL)
+        }
+    }
+
+    private func LinkToAppleMap(latitude: Double, longitude: Double, place: String) {
+        // Apple URL Scheme Reference: https://developer.apple.com/library/archive/featuredarticles/iPhoneURLScheme_Reference/MapLinks/MapLinks.html
+
+        // 애플 지도를 실행하고 전달된 위/경도 장소에 장소명으로 마커 표시
+        guard let url = URL(string: "http://maps.apple.com/?q=\(place)&ll=\(latitude),\(longitude)") else { return }
+
+        // canOpenURL 메서드를 통해 애플 지도 실행 URL이 true를 반환한다면 URL을 연결하여 앱 실행
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url)
         }
     }
 }
